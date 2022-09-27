7001a144686e29cd926cb5b2b8146fec8e62cabb
Friendosie <75134000+Friendosie@users.noreply.github.com>
2022-09-28 02:50:14 +0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='pyKeksik',
-      version='1.0',
+      version='1.1',
       url='https://github.com/Friendosie/pyKeksik',
       license='MIT',
       description='Неофициальная библиотека для работы с API https://keksik.io',
       packages=['pyKeksik'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Friendosie',
       install_requires=['requests'],
       author_email='friendosie@gmail.com',
