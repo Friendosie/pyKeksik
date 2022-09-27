0d9c0cfbd679164685df693bf7c1ef861bd6434a
Friendosie <75134000+Friendosie@users.noreply.github.com>
2022-09-28 01:45:09 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyKeksik',
+      version='1.0',
+      url='https://github.com/Friendosie/pyKeksik',
+      license='MIT',
+      description='Неофициальная библиотека для работы с API https://keksik.io',
+      packages=['pyKeksik'],
+      author='Friendosie',
+      install_requires=['requests'],
+      author_email='friendosie@gmail.com',
+      zip_safe=False)
